f16db1fc1575dbd629fa620b8a355396bcd2a50d
naocod <8162857@naver.com>
2023-04-06 14:40:00 +0900

    client design

---
+++ multichat_client/src/views/ClientApplication.java
@@ -0,0 +1,292 @@
+package views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.google.gson.Gson;
+
+import dto.request.RequestDto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.awt.CardLayout;
+import javax.swing.JTextField;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Map;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+@Getter
+public class ClientApplication extends JFrame {
+
+	private static final long serialVersionUID = -4753767777928836759L;
+	private static ClientApplication instance;
+
+	private Gson gson;
+	private Socket socket;
+
+	private JPanel mainPanel;
+	private CardLayout mainCard;
+
+	private JTextField usernameField;
+
+	private JTextField sendMessageField;
+
+	@Setter
+	private List<Map<String, String>> roomInfoList;
+	private DefaultListModel<String> roomNameListModel;
+	private DefaultListModel<String> usernameListModel;
+	private JList roomList;
+	private JList joinUserList;
+	
+	private JTextArea chttingContent;
+
+	public static ClientApplication getInstance() {
+		if (instance == null) {
+			instance = new ClientApplication();
+		}
+		return instance;
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientApplication frame = ClientApplication.getInstance();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ClientApplication() {
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				RequestDto<String> requestDto = new RequestDto<String>("exitRoom", null);
+				sendRequest(requestDto);
+			}
+		});
+
+		/* ================<< init >>================ */
+
+		gson = new Gson();
+		try {
+			socket = new Socket("192.168.2.101", 9090);
+			ClientRecive clientRecive = new ClientRecive(socket);
+			clientRecive.start();
+
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		} catch (ConnectException e1) {
+			JOptionPane.showMessageDialog(this, "서버에 접속할 수 없습니다.", "접속오류", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		/* ================<< frame set >>================ */
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(600, 150, 480, 800);
+
+		/* ================<< panel >>================ */
+
+		mainPanel = new JPanel();
+		JPanel loginPanel = new JPanel();
+		JPanel roomListPanel = new JPanel();
+		JPanel roomPanel = new JPanel();
+
+		/* ================<< layout >>================ */
+
+		mainCard = new CardLayout();
+
+		mainPanel.setLayout(mainCard);
+		loginPanel.setLayout(null);
+		roomListPanel.setLayout(null);
+		roomPanel.setLayout(null);
+
+		/* ================<< panel set >>================ */
+
+		setContentPane(mainPanel);
+		mainPanel.add(loginPanel, "loginPanel");
+		mainPanel.add(roomListPanel, "roomListPanel");
+		mainPanel.add(roomPanel, "roomPanel");
+
+		/* ================<< login panel >>================ */
+
+		JButton enterButton = new JButton("접속하기");
+
+		usernameField = new JTextField();
+		usernameField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					RequestDto<String> usernameCheckReqDto = 
+							new RequestDto<String>("usernameCheck", usernameField.getText());
+					sendRequest(usernameCheckReqDto);
+				}
+			}
+		});
+
+		usernameField.setBounds(62, 483, 319, 38);
+		loginPanel.add(usernameField);
+		usernameField.setColumns(10);
+
+		enterButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				RequestDto<String> usernameCheckReqDto = new RequestDto<String>("usernameCheck",
+						usernameField.getText());
+				sendRequest(usernameCheckReqDto);
+			}
+		});
+		enterButton.setBounds(62, 531, 319, 38);
+		loginPanel.add(enterButton);
+
+		/* ================<< roomList panel >>================ */
+
+		JScrollPane roomListScroll = new JScrollPane();
+		roomListScroll.setBounds(117, 0, 337, 751);
+		roomListPanel.add(roomListScroll);
+
+		roomNameListModel = new DefaultListModel<String>();
+		roomList = new JList(roomNameListModel);
+		roomList.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(e.getClickCount() == 2) {
+					int selectedIndex = roomList.getSelectedIndex();
+					RequestDto<Map<String, String>> requestDto = 
+							new RequestDto<Map<String,String>>("enterRoom", roomInfoList.get(selectedIndex));
+					sendRequest(requestDto);
+				}
+			}
+		});
+		roomListScroll.setViewportView(roomList);
+
+		JButton createRoomButton = new JButton("방 생성");
+		createRoomButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String roomName = null;
+				while (true) {
+					roomName = JOptionPane.showInputDialog(null, "생성할 방의 제목을 입력하세요.", "방 생성", JOptionPane.PLAIN_MESSAGE);
+					if(roomName == null) {
+						break;
+					}
+					if (!roomName.isBlank()) {
+						break;
+					}
+					JOptionPane.showMessageDialog(null, "공백은 사용할 수 없습니다.", "방 생성 오류", JOptionPane.ERROR_MESSAGE);
+				}
+
+				RequestDto<String> requestDto = new RequestDto<String>("createRoom", roomName);
+				sendRequest(requestDto);
+			}
+		});
+		createRoomButton.setBounds(12, 21, 93, 77);
+		roomListPanel.add(createRoomButton);
+
+		/* ================<< room panel >>================ */
+
+		JScrollPane joinUserListScroll = new JScrollPane();
+		joinUserListScroll.setBounds(0, 0, 341, 99);
+		roomPanel.add(joinUserListScroll);
+		
+		usernameListModel = new DefaultListModel<String>();
+		joinUserList = new JList(usernameListModel);
+		joinUserListScroll.setColumnHeaderView(joinUserList);
+
+		JButton roomExitButton = new JButton("나가기");
+		roomExitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(JOptionPane.showConfirmDialog(null, "정말로 방을 나가시겠습니까?", "방 나가기", JOptionPane.YES_NO_OPTION) == 0) {
+					RequestDto<String> requestDto = new RequestDto<String>("exitRoom", null);
+					sendRequest(requestDto);
+				}
+			}
+		});
+		roomExitButton.setBounds(340, 0, 114, 99);
+		roomPanel.add(roomExitButton);
+
+		JScrollPane chattingContentScroll = new JScrollPane();
+		chattingContentScroll.setBounds(0, 98, 454, 590);
+		roomPanel.add(chattingContentScroll);
+
+		chttingContent = new JTextArea();
+		chattingContentScroll.setViewportView(chttingContent);
+		chttingContent.setEditable(false);
+
+		sendMessageField = new JTextField();
+		sendMessageField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+					RequestDto<String> requestDto = new RequestDto<String>("sendMessage", sendMessageField.getText());
+					sendMessageField.setText("");
+					sendRequest(requestDto);
+				}
+			}
+		});
+		sendMessageField.setBounds(0, 688, 370, 63);
+		roomPanel.add(sendMessageField);
+		sendMessageField.setColumns(10);
+
+		JButton sendButton = new JButton("전송");
+		sendButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				RequestDto<String> requestDto = new RequestDto<String>("sendMessage", sendMessageField.getText());
+				sendMessageField.setText("");
+				sendRequest(requestDto);
+			}
+		});
+		
+		sendButton.setBounds(370, 688, 84, 63);
+		roomPanel.add(sendButton);
+
+	}
+
+	private void sendRequest(RequestDto<?> requestDto) {
+		String reqJson = gson.toJson(requestDto);
+		OutputStream outputStream = null;
+		PrintWriter printWriter = null;
+
+		try {
+			outputStream = socket.getOutputStream();
+			printWriter = new PrintWriter(outputStream, true);
+			printWriter.println(reqJson);
+			System.out.println("클라이언트 -> 서버: " + reqJson);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ multichat_server/src/main/ConnectedSocket.java
@@ -0,0 +1,227 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+import dto.request.RequestDto;
+import dto.response.ResponseDto;
+import entity.Room;
+import lombok.Getter;
+
+// 다른 객체가 username과 socket을 수정할 일이없어 setter가 필요없으므로 getter만 사용
+@Getter
+public class ConnectedSocket extends Thread {
+
+	private static List<ConnectedSocket> connectedSocketList = new ArrayList<>();
+	private static List<Room> roomList = new ArrayList<>();
+	private static int index = 0;
+	private Socket socket;
+	private String username;
+
+	private Gson gson;
+
+	public ConnectedSocket(Socket socket) {
+		this.socket = socket;
+//		connectedSocketList.add(this);
+		gson = new Gson();
+
+		// 임시 방 객체 생성
+//		Room room = new Room("Testroom" + index, "TestUser" + index);
+//		index++;
+//		roomList.add(room);
+	}
+
+	@Override
+	public void run() {
+
+		BufferedReader bufferedReader;
+
+		try {
+			while (true) {
+				bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String requestJson = bufferedReader.readLine();
+
+				System.out.println("요청: " + requestJson);
+				requestMapping(requestJson);
+
+			}
+		} catch (SocketException e) {
+			connectedSocketList.remove(this);
+			System.out.println(username + ": 클라이언트 종료");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void requestMapping(String requestJson) {
+		RequestDto<?> requestDto = gson.fromJson(requestJson, RequestDto.class);
+
+		Room room = null;
+
+		switch (requestDto.getResource()) {
+		case "usernameCheck":
+			checkUsername((String) requestDto.getBody());
+			break;
+		case "createRoom":
+			room = new Room((String) requestDto.getBody(), username);
+			room.getUsers().add(this);
+			roomList.add(room);
+			sendToMe(new ResponseDto<String>("createRoomSuccessfully", null));
+			refreshUsernameList(room);
+			sendToAll(refeshRoomList(), connectedSocketList);
+			break;
+
+		case "enterRoom":
+			room = findRoom((Map<String, String>) requestDto.getBody());
+			room.getUsers().add(this);
+			sendToMe(new ResponseDto<String>("enterRoomSuccessfully", null));
+			refreshUsernameList(room);
+			break;
+
+		case "sendMessage":
+			room = findConnectedRoom(username);
+			sendToAll(new ResponseDto<String>("reciveMessage", username + " >>> " + (String) requestDto.getBody()),
+					room.getUsers());
+			break;
+
+		case "exitRoom":
+			room = findConnectedRoom(username);
+			try {
+				// 방장일 때
+				if (room.getOwner().equals(username)) {
+					exitRoomAll(room);
+					// 방장 아닐 때
+				} else {
+					exitRoom(room);
+				}
+			} catch (NullPointerException e) {
+				System.out.println("클라이언트 강제 종료됨");
+			}
+
+			break;
+		}
+	}
+
+	private void checkUsername(String username) {
+		if (username.isBlank()) {
+			sendToMe(new ResponseDto<String>("usernameCheckIsBlank", "사용자 이름은 공백일 수 없습니다."));
+			return;
+		}
+
+		for (ConnectedSocket connectedSocket : connectedSocketList) {
+			if (connectedSocket.getUsername().equals(username)) {
+				sendToMe(new ResponseDto<String>("usernameCheckIsDuplicate", "이미 사용중인 이름입니다."));
+				return;
+			}
+		}
+
+		this.username = username;
+		connectedSocketList.add(this);
+		sendToMe(new ResponseDto<String>("usernameCheckSuccessfully", null)); // 이때 응답이 오면 cardLayout을 바꿔줌
+		sendToMe(refeshRoomList());
+	}
+
+	private ResponseDto<List<Map<String, String>>> refeshRoomList() {
+		List<Map<String, String>> roomNameList = new ArrayList<>();
+
+		for (Room room : roomList) {
+			Map<String, String> roomInfo = new HashMap<>();
+			roomInfo.put("roomName", room.getRoomName());
+			roomInfo.put("owner", room.getOwner());
+
+			roomNameList.add(roomInfo);
+		}
+		ResponseDto<List<Map<String, String>>> responseDto = new ResponseDto<List<Map<String, String>>>(
+				"refreshRoomList", roomNameList);
+		return responseDto;
+	}
+
+	private Room findConnectedRoom(String username) {
+		Room room = null;
+		for (Room r : roomList) {
+			for (ConnectedSocket cs : r.getUsers()) {
+				if (cs.getUsername().equals(username)) {
+					return r;
+				}
+			}
+		}
+		return null;
+	}
+
+	private Room findRoom(Map<String, String> roomInfo) {
+		for (Room room : roomList) {
+			if (room.getRoomName().equals(roomInfo.get("roomName")) && room.getOwner().equals(roomInfo.get("owner"))) {
+				return room;
+			}
+		}
+		return null;
+	}
+
+	private void refreshUsernameList(Room room) {
+		List<String> usernameList = new ArrayList<>();
+		usernameList.add("방제목: " + room.getRoomName());
+		for (ConnectedSocket connectedSocket : room.getUsers()) {
+			if (connectedSocket.getUsername().equals(room.getOwner())) {
+				usernameList.add(connectedSocket.getUsername() + "(방장)");
+				continue;
+			}
+			usernameList.add(connectedSocket.getUsername());
+		}
+		ResponseDto<List<String>> responseDto = new ResponseDto<List<String>>("refreshUsernameList", usernameList);
+		sendToAll(responseDto, room.getUsers());
+	}
+
+	private void exitRoomAll(Room room) {
+		sendToAll(new ResponseDto<String>("exitRoom", null), room.getUsers());
+		roomList.remove(room);
+		sendToAll(refeshRoomList(), connectedSocketList);
+	}
+
+	private void exitRoom(Room room) {
+		room.getUsers().remove(this);
+		sendToMe(new ResponseDto<String>("exitRoom", null));
+		refreshUsernameList(room);
+	}
+
+	private void sendToMe(ResponseDto<?> responseDto) {
+		try {
+			OutputStream outputStream = socket.getOutputStream();
+			PrintWriter printWriter = new PrintWriter(outputStream, true);
+
+			String responseJson = gson.toJson(responseDto);
+			printWriter.println(responseJson);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void sendToAll(ResponseDto<?> responseDto, List<ConnectedSocket> connectedSockets) {
+		for (ConnectedSocket connectedSocket : connectedSockets) {
+			try {
+				OutputStream outputStream = connectedSocket.getSocket().getOutputStream();
+				PrintWriter printWriter = new PrintWriter(outputStream, true);
+
+				String responseJson = gson.toJson(responseDto);
+				printWriter.println(responseJson);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+}
